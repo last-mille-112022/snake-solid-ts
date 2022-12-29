8010e5db4ac4a8aa23c8131c139ae62554ed12bc
TheGuzman <guzmanmedrano.diego@gmail.com>
2022-12-29 13:07:46 +0100

    feat(menu-printer): add menu printer interface and console menu printer class

---
+++ src/ui/menu-printer/console-menu-printer.ts
@@ -0,0 +1,36 @@
+import * as readline from 'node:readline/promises';
+import type { MenuPrinter } from './menu-printer.model';
+
+export class ConsoleMenuPrinter implements MenuPrinter {
+  #options: string[];
+
+  constructor(options: string[]) {
+    this.#options = options;
+  }
+
+  setOptions(options: string[]) {
+    this.#options = options;
+  }
+
+  printMenuOptions(): void {
+    let optionString = '';
+    this.#options.forEach((question, index) => {
+      optionString += `
+${index + 1}. ${question} `;
+    });
+    console.log(`\nSNAKE MENU OPTIONS\n ${optionString}`);
+  }
+
+  async readUserAnswer(): Promise<string> {
+    const rl = readline.createInterface(process.stdin, process.stdout);
+    let answer = 0;
+    this.printMenuOptions();
+    answer = Number(await rl.question('\nChoose an option '));
+    while (answer < 1 || answer > this.#options.length + 1 || isNaN(answer)) {
+      // eslint-disable-next-line no-await-in-loop
+      answer = Number(await rl.question('\nPlease choose a valid option '));
+    }
+
+    return this.#options[answer - 1];
+  }
+}
