36f21840d47ddc4ab09ea7fa53e87aee86a9897d
davzlum <davzlum@gmail.com>
2023-01-10 18:55:22 +0100

    implemented checklist method and started testing

--- src/ui/item-render/item-render-engine.ts
+++ src/ui/item-render/item-render-engine.ts
@@ -1,27 +1,37 @@
-import type { Drawable, SnakeRenderEngine } from '../render-engine';
+import type { Item } from '../../game/items/item.interface';
+import type { SnakeRenderEngine } from '../render-engine';
 
 export class ItemRenderEngine {
   #board;
-  #itemList: Drawable[];
+  #itemList: Item[];
 
   constructor(renderEngine: SnakeRenderEngine) {
     this.#board = renderEngine;
     this.#itemList = [];
   }
 
-  addItem(item: Drawable) {
+  addItem(item: Item) {
     this.#itemList.push(item);
+    this.renderItems();
+    return this.#itemList;
   }
 
   renderItems() {
     // eslint-disable-next-line
     this.#itemList.forEach((element) => {
       this.#board.drawElement(element);
+      this.#board.render();
     });
   }
 
+  // Se ejecutará a cada frame del juego, iterará sobre la #itemList y llamará al método "checkLifespan" de cada objeto.
+  // Eliminará del array los ítems cuyo método "checkLifespan" devuelva true. Si no están en el array, no se renderizan.
   checkItemList() {
-    // Se ejecutará a cada frame del juego, iterará sobre la #itemList y llamará al método "checkLifespan" de cada objeto.
-    // Eliminará del array los ítems cuyo método "checkLifespan" devuelva true. Si no están en el array, no se renderizan.
+    this.#itemList.forEach((item, i) => {
+      if (item.checkLifespan()) {
+        this.#itemList.slice(i, 1);
+      }
+    });
+    return this.#itemList;
   }
 }
