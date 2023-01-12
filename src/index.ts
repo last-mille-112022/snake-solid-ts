00995782f5a3f27dcf169e0d100ef52463155123
Natalia <natirgh@gmail.com>
2023-01-12 16:49:07 +0100

    feat(index): add game menu to start and end game in index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { GameController } from './game/game-controller.js';
+import MenuItem from './ui/menu-item/menu-item.js';
+import { ConsoleMenuPrinter } from './ui/menu-printer/console-menu-printer.js';
+import SnakeMenu from './ui/menu/snake-menu/snake-menu.js';
 
 const game = new GameController();
-game.start();
+const printer = new ConsoleMenuPrinter();
+const startGameItem = new MenuItem('Empezar partida', game);
+const endGameItem = new MenuItem('Salir', game);
+const gameMenu = new SnakeMenu([startGameItem, endGameItem], printer, 'SNAKE MENU');
+void gameMenu.printOptions();
+
